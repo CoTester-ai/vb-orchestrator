613e558e27c45fadcf3578c09b15d05593d3764d
chertkov.andrey3 <chertkov.andrey3@wb.ru>
2024-08-14 13:52:38 +0200

    Set one possible image

--- internal/config/room.go
+++ internal/config/room.go
@@ -65,18 +65,7 @@ func (Room) Init(cmd *cobra.Command) error {
 	}
 
 	cmd.PersistentFlags().StringSlice("neko_images", []string{
-		"m1k1o/neko:firefox",
-		"m1k1o/neko:chromium",
-		"m1k1o/neko:google-chrome",
-		"m1k1o/neko:ungoogled-chromium",
-		"m1k1o/neko:microsoft-edge",
-		"m1k1o/neko:brave",
-		"m1k1o/neko:vivaldi",
-		"m1k1o/neko:tor-browser",
-		"m1k1o/neko:vlc",
-		"m1k1o/neko:remmina",
-		"m1k1o/neko:xfce",
-		"m1k1o/neko:kde",
+		"achertkov/cotester:v0.0.1-alpha.1",
 	}, "neko images to be used")
 	if err := viper.BindPFlag("neko_images", cmd.PersistentFlags().Lookup("neko_images")); err != nil {
 		return err
